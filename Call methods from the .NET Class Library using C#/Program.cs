df5626490e8ba958d3538e5c16d891d1061efecd
Cameron Jones <jones_cameron@student.mahoningctc.com>
2024-11-18 11:39:41 -0500

    Add project files.

---
+++ Call methods from the .NET Class Library using C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_methods_from_the.NET_Class_Library_using_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue;
+            largerValue = Math.Max(firstValue, secondValue);
+            Console.Write(largerValue);
+
+            Console.ReadLine();
+        }
+    }
+}
